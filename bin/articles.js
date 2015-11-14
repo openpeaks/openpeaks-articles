16a4aaec10ee036ef61b245de3d6f6b9895b4e54
Dumitru Cantea <canteadumitru@gmail.com>
2015-11-14 19:10:54 +0200

    filter articles by last changed date

--- bin/articles.js
+++ bin/articles.js
@@ -8,21 +8,31 @@ var pkg = require('../package.json');
 var program = require('commander');
 program.version(pkg.version);
 
+function clearOptions(options) {
+	for (var prop in options) {
+		if (typeof options[prop] !== 'string') {
+			delete options[prop];
+		}
+	}
+	return options;
+}
+
 program
-	.command('generate [lang] [name]')
+	.command('generate [lang]')
 	.description('Generates articles')
 	.option('-l, --lang lang', 'Articles language')
-	.option('-n, --name [name]', 'Article file/template name')
+	.option('-n, --name [name]', 'Article file/template name', /^[\w\d-]+$/)
 	.option('-t, --time [time]', 'Filter articles by last updated date: 30s, 10m, 2d')
 	.option('-o, --output [output]', 'Output directory')
-	.action(function(lang, name, opts) {
+	.action(function(lang, opts) {
 		var options = {
 			lang: lang || opts.lang,
-			name: name || opts.name,
+			name: opts.name,
 			output: opts.output,
 			time: opts.time
 		};
-		console.log('options', options);
+		options = clearOptions(options);
+		console.log('options:', options);
 		var action = require('../lib/actions/generator').start;
 		return action(options);
 	});
@@ -37,7 +47,8 @@ program
 			lang: lang || opts.lang,
 			name: name || opts.name
 		};
-		console.log('options', options);
+		options = clearOptions(options);
+		console.log('options:', options);
 		var action = require('../lib/actions/validator').start;
 		return action(options);
 	});
--- data/articles/ro/mobtel-by-country.md
+++ data/articles/ro/mobtel-by-country.md
@@ -2,7 +2,7 @@
 model: mobtel_per_100
 title: "<%= country.name.common %>: abonamente de telefonie mobilă la 100 locuitori"
 slug: "<%= country.name.common %>: abonamente de telefonie mobilă la 100 locuitori"
-target: country:md
+target: country
 ---
 
 Evoluția numărului de abonamente de telefonie mobile și celulare la 100 locuitori în <%= country.name.common %>.
